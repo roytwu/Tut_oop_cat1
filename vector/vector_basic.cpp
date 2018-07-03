87e0a18affbc545bc89daab16ad90233e67f1100
roy <tsehuaiwu@gmail.com>
2018-07-02 19:51:47 -0700

    use ostream_iterator and copy to output a vector. Adding comments in ' vector_basic.cpp'

--- vector/vector_basic.cpp
+++ vector/vector_basic.cpp
@@ -4,25 +4,47 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm> //copy
+#include <iterator> //ostream_iterator
 using std::cout;
 using std::endl;
 using std::string;
 using std::vector;
+using std::copy;
+using std::ostream_iterator;
 
 vector<string>GetString();
 
 
 int main(){
 	size_t size = 10;
+	//Element count
 	vector<int> vec0; //declare an empty vector
 	vector<int> vec1(size); //declare a vector of size 10 with all values as 0
+
+	//list initializer: use the value inside the curly braces as a list of element 
 	vector<int> vec2(size, 99); //declare a vector of size 10 with all values as 99
 	for(unsigned int i=0; i<size; i++){
 		vec1[i] = i;
 		vec2[i] = vec2[i]+i;
 	}
 	cout << "vec1[1] is " << vec1[1] << endl;
-	cout << "vec2[1] is " << vec2[1] << endl;
+	cout << "vec2[1] is " << vec2[1] << endl << endl;
+
+	vector<int> vecA(10,1); //vecA has 10 elements with value 1 (Element COunt)
+	vector<int> vecB{10,1}; //vecB has 2 elements with values 10 and 1 (List Initializer)
+	cout << "vecA is ... ";
+	copy(vecA.begin(), vecA.end(), ostream_iterator<int>(cout, " "));
+	cout << endl;
+
+	cout << "vecB is ... ";
+	copy(vecB.begin(), vecB.end(), ostream_iterator<int>(cout, " "));
+	cout << endl;
+
+	vecA = vecB;  //copy elements of vecB into vecA
+	cout << "After copy, vecA is ... ";
+	copy(vecA.begin(), vecA.end(), ostream_iterator<int>(cout, " "));
+	cout << endl << endl;
 
 	//initialize a vector like array
 	vector<double> vec3{1.1, 2.2, 3.3}; 
@@ -50,6 +72,7 @@ int main(){
 }
 
 
+
 vector<string> GetString(){
 	vector<string> str;
 	str.push_back("One");
@@ -57,3 +80,8 @@ vector<string> GetString(){
 	str.push_back("Three");
 	return str;
 }
+
+
+/*-----Note-----*/
+//Define a vector of specific size can result in poor performance
+//Starting with an empty vector and adding elements at run time is a better approach
