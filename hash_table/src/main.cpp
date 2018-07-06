dab0044216fc43df0c852088131f5e3118588af9
roy <tsehuaiwu@gmail.com>
2018-07-05 20:13:12 -0700

    test unordered_map

--- hash_table/src/main.cpp
+++ hash_table/src/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <unordered_map>
+#include <utility>  //make_pair()
 #include "hash.h"
 using std::cout;
 using std::endl;
+using std::unordered_map;
+using std::make_pair;
 
 int main(){
 
@@ -19,10 +23,29 @@ int main(){
 	o_hash.insertData(Dict("Darth", "Vader"));
 
 	o_hash.displayTb();
-	//cout << "Obi-Wan's last name is.. " << o_hash.search("Obi-Wan") << endl;
+	cout << "Obi-Wan's last name is.. " << o_hash.search("Obi-Wan") << endl << endl;
 
-	o_hash.deleteData("Luke"); //simething is wrong here
+	o_hash.deleteData("Luke"); 
 	o_hash.displayTb();
+
+	//unordered_map is already a hash table 
+	unordered_map<string, string> umap;
+	umap["Han"] = "Solo";
+	umap["Anakin"] = "Skywalker";
+	umap["Obi-Wan"] = "Kenobi";
+	umap["Luke"] = "Skywalker";
+	umap.insert(make_pair("Darth", "Vader"));
+
+	unordered_map<string, string>::iterator itr;
+	cout << "----- ----- unordered_map ----- -----" << endl;
+    cout << "All Elements in umap : \n";
+
+  
+    	
+    for (itr = umap.begin(); itr != umap.end(); itr++){
+        	cout << "(" <<itr->first << "  " << itr->second << ")" << endl;
+    }
+	
 
 	return 0;
 }
