da28b90d5015d8d58b87c2b6e3919a0b03079492
Roy <tsehuaiwu@gmail.com>
2018-07-11 08:57:49 -0700

    comparing ofstream and fstream

--- file/main.cpp
+++ file/main.cpp
@@ -3,22 +3,41 @@
 #include <fstream>
 using std::cout;
 using std::endl;
-using std::string;
 using std::ofstream;
+using std::string;
+
+using std::fstream;
+using std::ios;
+
 
 int main(){
 	cout << "Program starts here....." << endl;
 	
-	ofstream outFile;
-	string fileName = "demoTextFile.txt";
-	outFile.open(fileName);
+	ofstream outFile1; //output_file_stream
+	fstream  outFile2;
 
-	if(outFile.is_open()){
-		outFile << "Go Go Power Rangers!!!" << endl;
-		outFile << 12345 << endl;
-		outFile.close();
+	string fileName1 = "demoFile1.txt";
+	string fileName2 = "demoFile2.txt";
+
+	outFile1.open(fileName1);
+	outFile2.open(fileName2, ios::out);
+
+
+	if(outFile1.is_open()){
+		outFile1 << "Writing file with ofstream!" << endl;
+		outFile1 << 12345 << endl;
+		outFile1.close();
 	} else{
-		cout << "Could not create file: " << fileName << endl;
+		cout << "Could not create file: " << fileName1 << endl;
+	}
+
+
+	if(outFile2.is_open()){
+		outFile2 << "Write file with fstream..." << endl;
+		outFile2 << 999.99 << endl;
+		outFile2.close();
+	} else{
+		cout << "Could not create file: " << fileName2 << endl;
 	}
 
 	return 0;
