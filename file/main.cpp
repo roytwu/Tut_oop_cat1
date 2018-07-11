c80d59e25bf23a3c172b05a3d434a3f0dabb80e0
Roy <tsehuaiwu@gmail.com>
2018-07-11 10:50:48 -0700

    adding comments about <fstream>

--- file/main.cpp
+++ file/main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <string>
-#include <fstream>
+#include <fstream> //fstream, ofstream, ios
 using std::cout;
 using std::endl;
-using std::ofstream;
 using std::string;
-
+using std::ofstream; 
 using std::fstream;
 using std::ios;
 
@@ -13,14 +12,14 @@ using std::ios;
 int main(){
 	cout << "Program starts here....." << endl;
 	
-	ofstream outFile1; //output_file_stream
+	ofstream outFile1; //outFile1 is an output_file_stream object
 	fstream  outFile2;
 
 	string fileName1 = "demoFile1.txt";
 	string fileName2 = "demoFile2.txt";
 
 	outFile1.open(fileName1);
-	outFile2.open(fileName2, ios::out);
+	outFile2.open(fileName2, ios::out);  //see Note for reference
 
 
 	if(outFile1.is_open()){
@@ -42,3 +41,27 @@ int main(){
 
 	return 0;
 }
+
+
+/* ----- Note -----
+ofstream: output file stream  
+	ofstream only has methods for outputting
+fstream: input/output file stream class
+	fstream can be used for input and output
+
+<fstream>
+void open(): open file
+void close(): close file
+bool is_open(): true if a file is open and associated with the stream 
+                object, false otherwise 
+
+open(file_name, mode);
+mode is a parameter with a combination of the foloowing flags:
+	ios::in              
+	ios::out
+	ios::binary                
+	ios::ate
+	ios::app
+	ios::trunc
+All these flags can be combined using bitwise operator OR(|)
+*/
