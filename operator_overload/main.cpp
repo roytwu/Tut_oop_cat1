ad4317673920f86be456bd30aebca31e2004583c
Roy <roywu2018@gmail.com>
2018-12-08 00:33:53 -0500

    adding empty operator_overload project

---
+++ operator_overload/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+
+
+using std::cout; 
+using std::endl;
+using std::string;
+
+class Foo {
+public:
+	virtual void operator()() {
+		cout << "foo!" << endl;
+	}
+};
+
+class Bar : public Foo {
+public:
+	virtual void operator()() {
+		cout << "bar!" << endl;
+	}
+};
+
+
+
+int main()
+{
+	const string cmake_system_name = SYSTEM_NAME;
+	cout << "Hello from " << cmake_system_name << endl << endl;
+
+	Bar o_bar;
+
+	//using pointer
+	Foo *ptr = &o_bar;
+	(*ptr)(); //clumsy! - prints bar!
+
+	//using reference 
+	Foo &ref = o_bar;
+	ref(); //better - prints bar!
+
+	//also correct
+	o_bar(); //prints bar!
+
+    return 0;
+}
+
