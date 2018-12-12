6174d01367ac08032dec285d9b1853778c04de7a
roytwu <roywu2018@gmail.com>
2018-12-12 12:22:05 -0500

    upadtng CSVReader class, adding a .csv file

--- vector/read_from_CSV/main.cpp
+++ vector/read_from_CSV/main.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
-#include <string>
+#include <vector>
 #include "readCSV.hpp"
 
 using std::cout; 
 using std::endl;
-using std::string;
-
+using std::vector;
 
 int main()
 {
+	CSVReader o_reader("imu_log.csv");
 
+	vector< vector<std::string> > v_dataList = o_reader.getData();
 
     return 0;
 }
--- vector/read_from_CSV/readCSV.hpp
+++ vector/read_from_CSV/readCSV.hpp
@@ -1,6 +1,11 @@
+#ifndef READCSV_H_
+#define READCSV_H_
+
+#include <boost/algorithm/string.hpp>
+#include <fstream>
 #include <string>
 #include <vector>
-#include <fstream>
+
 using std::string;
 using std::vector;
 using std::ifstream;
@@ -9,24 +14,33 @@ using std::getline;
 class CSVReader{
 public:
 	CSVReader(string filename, string delim = ","): fileName(filename), delimeter(delim) {}
-	vector< vector<string> > getData(); //function to fetch data from a CSV file
+
+	//function to fetch data from a CSV file
+	vector< vector<string> > getData(); 
 	
 private:	
-	string fileNmae;
+	string fileName;
 	string delimeter;
 };
 
-/* 
-Parses through csv file line by line and returns the data in vector of vector of strings  
-*/
+
+// Parses through csv file line by line and returns the data in vector of vector of strings  
 vector< vector<string> > CSVReader::getData(){
 	ifstream o_input;
-	o_inpupt.open(fileName);
+	o_input.open(fileName);
 	
 	vector< vector<string> > dataList;
 	
 	string line;
-//	while(getline(o_input, line)){
-		
-//	}
+	while(getline(o_input, line)){
+		vector<string> v_foo;
+		boost::algorithm::split(v_foo, line, boost::algorithm::is_any_of(delimeter));
+		dataList.push_back(v_foo);
+	}
+	o_input.close();
+
+	return dataList;
 }
+
+
+#endif  //READCSV_H_
