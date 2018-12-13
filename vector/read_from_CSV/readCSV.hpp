560fb9ec6e28a3c53cf4fc02bc095015707cbe2f
roytwu <roywu2018@gmail.com>
2018-12-13 17:29:07 -0500

    updating constructor of class CSVReader

--- vector/read_from_CSV/readCSV.hpp
+++ vector/read_from_CSV/readCSV.hpp
@@ -21,21 +21,31 @@ using std::getline;
 
 class CSVReader{
 public:
-	CSVReader(string filename, string delim = ","): fileName(filename), delimeter(delim) {}
+	//* inline member initialization:
+	// CSVReader(string filename, string delim = ","): fileName(filename), delimeter(delim) {}
+	//* which is equivalent to:
+	// CSVReader(string filename, string delim = ","): {fileName=filename; delimeter=delim;}  
+	CSVReader(string, string);
 
-	//function to fetch data from a CSV file
+	//* function to fetch data from a CSV file
 	vector< vector<string> > getData(); 
 	
 private:	
-	string fileName;
-	string delimeter;
+	string ms_fileName;
+	string ms_delimeter;
 };
 
 
-// Parses through csv file line by line and returns the data in vector of vector of strings  
+//* Constructor Overloading
+CSVReader::CSVReader(string filename, string delim = ",") {
+	ms_fileName = filename;  
+	ms_delimeter = delim;
+}
+
+//* Parses through csv file line by line and returns the data in vector of vector of strings  
 vector< vector<string> > CSVReader::getData(){
 	ifstream o_inFile;
-	o_inFile.open(fileName);
+	o_inFile.open(ms_fileName);
 	
 	vector< vector<string> > dataList;
 	
@@ -44,12 +54,12 @@ vector< vector<string> > CSVReader::getData(){
 	if (o_inFile.is_open()) {
 		while (getline(o_inFile, line)) {
 			vector<string> v_foo;
-			boost::algorithm::split(v_foo, line, boost::algorithm::is_any_of(delimeter));
+			boost::algorithm::split(v_foo, line, boost::algorithm::is_any_of(ms_delimeter));
 			dataList.push_back(v_foo);
 		}
 		o_inFile.close();
 	} else {
-		cout << "Could not open file" << fileName << endl;
+		cout << "Could not open file" << ms_fileName << endl;
 	}
 
 
