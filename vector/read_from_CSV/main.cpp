8fbc165c210d6f60cdd8c93dfb22222e88de1471
roytwu <roywu2018@gmail.com>
2018-12-13 10:41:31 -0500

    updating CSV reader

--- vector/read_from_CSV/main.cpp
+++ vector/read_from_CSV/main.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
+//#include <iostream>
 #include <vector>
 #include "readCSV.hpp"
 
-using std::cout; 
-using std::endl;
+//using std::cout; 
+//using std::endl;
 using std::vector;
 
 int main()
@@ -11,6 +11,13 @@ int main()
 	CSVReader o_reader("imu_log.csv");
 
 	vector< vector<std::string> > v_dataList = o_reader.getData();
+
+	for (vector<std::string> vec : v_dataList) {
+		for (std::string data : vec) {
+			cout << data << ", ";
+		}
+		cout << endl;
+	}
 
     return 0;
 }
--- vector/read_from_CSV/readCSV.hpp
+++ vector/read_from_CSV/readCSV.hpp
@@ -3,9 +3,12 @@
 
 #include <boost/algorithm/string.hpp>
 #include <fstream>
+#include <iostream>
 #include <string>
 #include <vector>
 
+using std::cout;
+using std::endl;
 using std::string;
 using std::vector;
 using std::ifstream;
@@ -26,18 +29,24 @@ private:
 
 // Parses through csv file line by line and returns the data in vector of vector of strings  
 vector< vector<string> > CSVReader::getData(){
-	ifstream o_input;
-	o_input.open(fileName);
+	ifstream o_inFile;
+	o_inFile.open(fileName);
 	
 	vector< vector<string> > dataList;
 	
 	string line;
-	while(getline(o_input, line)){
-		vector<string> v_foo;
-		boost::algorithm::split(v_foo, line, boost::algorithm::is_any_of(delimeter));
-		dataList.push_back(v_foo);
+
+	if (o_inFile.is_open()) {
+		while (getline(o_inFile, line)) {
+			vector<string> v_foo;
+			boost::algorithm::split(v_foo, line, boost::algorithm::is_any_of(delimeter));
+			dataList.push_back(v_foo);
+		}
+		o_inFile.close();
+	} else {
+		cout << "could not open file" << fileName << endl;
 	}
-	o_input.close();
+
 
 	return dataList;
 }
