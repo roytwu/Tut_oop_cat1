6e4720cd71142c4721655647bce2989bb93ce4a0
roytwu <roywu2018@gmail.com>
2019-01-22 11:47:53 -0500

    adding sub-project enum

---
+++ _cpp basic/enum/main.cpp
@@ -0,0 +1,18 @@
+/* file name: main.cpp
+ * programmer: Roy T Wu
+ * Project: enumeration */
+
+#include <iostream>
+#include <vector>
+#include <string>
+using std::cout; 
+using std::endl;
+using std::vector;
+using std::string; 
+
+int main()
+{
+    cout << "Hello World" << endl;
+    return 0;
+}
+
