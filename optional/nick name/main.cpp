b7db4d6e2489208c6878b8f03a0660a584f6f5ee
roytwu <roywu2018@gmail.com>
2019-01-23 11:49:00 -0500

    adding projects about std::optional

---
+++ optional/nick name/main.cpp
@@ -0,0 +1,21 @@
+/* file name: main.cpp
+ * programmer: Roy T Wu
+ * Project: tutorial for std::optional
+ * Credit: https://www.bfilipek.com/2018/05/using-optional.html
+ */
+
+#include <iostream>
+#include <vector>
+#include <string>
+using std::cout; 
+using std::endl;
+using std::vector;
+using std::string; 
+
+
+int main()
+{
+
+    return 0;
+}
+
