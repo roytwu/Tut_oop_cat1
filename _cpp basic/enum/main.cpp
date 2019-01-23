0f33cfe50853a36673d8cef32e3b841b6c3ffa9b
roytwu <roywu2018@gmail.com>
2019-01-23 15:37:13 -0500

    adding example of boost::optional

--- _cpp basic/enum/main.cpp
+++ _cpp basic/enum/main.cpp
@@ -10,9 +10,26 @@ using std::endl;
 using std::vector;
 using std::string; 
 
+namespace RoySpace {
+	enum class Color_A {
+		bronze, silver, gold
+	};
+
+	enum class Color_B {
+		bronze, silver, gold, red, green, blue
+	};
+}
+
+
+using namespace RoySpace;
+
 int main()
 {
     cout << "Hello World" << endl;
+	Color_A clrA = Color_A::gold; 
+	Color_B clrB = Color_B::gold;
+
+	cout << clrA << endl;
     return 0;
 }
 
--- optional/nick name/main.cpp
+++ optional/nick name/main.cpp
@@ -2,20 +2,57 @@
  * programmer: Roy T Wu
  * Project: tutorial for std::optional
  * Credit: https://www.bfilipek.com/2018/05/using-optional.html
+ * Note: std::optional is not incorporated in VS2015, here boost::optional is applied
  */
 
 #include <iostream>
-#include <vector>
 #include <string>
+#include <boost/optional.hpp>
+
 using std::cout; 
 using std::endl;
-using std::vector;
 using std::string; 
+
+class UserRecord {
+public:
+	UserRecord(const string& name, const boost::optional<string> nick, boost::optional<int> age)
+		: mName( name )
+		, mNick( nick )
+		, mAge( age )
+	{}
+
+	friend std::ostream & operator << (std::ostream& stream, const UserRecord& user);
+
+private:
+	string mName;
+	const boost::optional<string> mNick;
+	boost::optional<int> mAge;
+};
+
+std::ostream& operator << (std::ostream& os, const UserRecord& user) {
+	os << user.mName << " ";
+	if (user.mNick) {
+		os << *user.mNick << " ";
+	}
+
+	if (user.mAge) {
+		os << "age of " << *user.mAge;
+	}
+	return os;
+}
+
+
+
 
 
 int main()
 {
+	UserRecord tim{ "Tim", string("SuperTim"), 16};
+	UserRecord nano { "Nathan", boost::none, boost::none };
 
+	cout << tim << endl;
+	cout << nano << endl;
     return 0;
+
 }
 
