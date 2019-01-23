258ef33e1e874cc1df3e998b5d9d66c31e78bf94
roytwu <roywu2018@gmail.com>
2019-01-23 15:52:42 -0500

    adding comments

--- optional/nick name/main.cpp
+++ optional/nick name/main.cpp
@@ -1,8 +1,9 @@
-/* file name: main.cpp
- * programmer: Roy T Wu
- * Project: tutorial for std::optional
- * Credit: https://www.bfilipek.com/2018/05/using-optional.html
- * Note: std::optional is not incorporated in VS2015, here boost::optional is applied
+/* File name: main.cpp
+ * Developer: Roy T Wu
+ * Project:   tutorial for std::optional, 
+              user name with an optional nickname and age
+ * Credit:    https://www.bfilipek.com/2018/05/using-optional.html
+ * Note:      std::optional is not incorporated in VS2015, here boost::optional is applied
  */
 
 #include <iostream>
@@ -15,9 +16,9 @@ using std::string;
 
 class UserRecord {
 public:
-	UserRecord(const string& name, const boost::optional<string> nick, boost::optional<int> age)
+	UserRecord(const string& name, const boost::optional<string> nickName, boost::optional<int> age)
 		: mName( name )
-		, mNick( nick )
+		, mNickName( nickName )
 		, mAge( age )
 	{}
 
@@ -25,14 +26,14 @@ public:
 
 private:
 	string mName;
-	const boost::optional<string> mNick;
+	const boost::optional<string> mNickName;
 	boost::optional<int> mAge;
 };
 
 std::ostream& operator << (std::ostream& os, const UserRecord& user) {
 	os << user.mName << " ";
-	if (user.mNick) {
-		os << *user.mNick << " ";
+	if (user.mNickName) {
+		os << *user.mNickName << " ";
 	}
 
 	if (user.mAge) {
@@ -42,17 +43,14 @@ std::ostream& operator << (std::ostream& os, const UserRecord& user) {
 }
 
 
-
-
-
 int main()
 {
-	UserRecord tim{ "Tim", string("SuperTim"), 16};
+	//* only one level of conversion works, so "SuperTim" must be wrapped by std::string
+	UserRecord tim ( "Tim", string("SuperTim"), 16);
 	UserRecord nano { "Nathan", boost::none, boost::none };
 
 	cout << tim << endl;
 	cout << nano << endl;
     return 0;
-
 }
 
