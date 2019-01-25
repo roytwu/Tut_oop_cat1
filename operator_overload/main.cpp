abbb510eeab4181594ad0f3e74794e477e03c328
roytwu <roywu2018@gmail.com>
2019-01-25 13:54:29 -0500

    updting optOverload porject

--- operator_overload/main.cpp
+++ operator_overload/main.cpp
@@ -1,3 +1,10 @@
+//* File name:   main.cpp (optOverload)
+//* Developer:   Roy Wu
+//* Last upate:  01/25/2019
+//* Description: 
+//*    1. illustrating basic concepts of operator overloading
+//*    2. demonstrating assignment operator (=) overloading and compare it with copy constructor
+
 #include <iostream>
 #include <string>
 
@@ -29,7 +36,22 @@ public:
 	Test(int id, string name): id(id), name(name){}
 
 	void print() {
-		cout << id << ": " << name << endl;
+		cout << id << ": " << name << endl << endl;
+	}
+
+	//* overloading assignment operator
+	const Test &operator=(const Test &other) {
+		cout << "Assignment running!" << endl;
+		id = other.id + 1;
+		name = other.name;
+		return *this;
+	}
+
+	//* copy constructor
+	Test(const Test &other) {
+		cout << "Copy constructor running!" << endl;
+		id = other.id + 10;
+		name = other.name;
 	}
 
 private:
@@ -59,9 +81,21 @@ int main(){
 	Test o_test1(01, "Tiguan");
 	Test o_test2(02, "Atlas");
 	Test o_test3(03, "Passat");
+
 	o_test1.print();
 	
+	o_test2 = o_test1; //* shallow copy, copy all the valus from o_test1 to o_test2
+	o_test2.print();
 	
+	o_test3.operator= (o_test2); //* equlas to o_test3 = o_test2
+	o_test3.print();
+
+	//* If you take an object and not giving initial values, 
+	//* and immediately assign this object to another object, it do not ues the assignment operator (=)
+	//* it is using copy constructor
+	Test o_test4 = o_test1; //* copy initilization, implicit invoking copy consturctor
+	o_test4.print();
+
 	
 	return 0;
 }
