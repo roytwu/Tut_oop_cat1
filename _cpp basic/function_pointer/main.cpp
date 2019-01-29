f267c3bd498fdb7767dcd2168bdaee44421a1de1
roytwu <roywu2018@gmail.com>
2019-01-29 11:50:35 -0500

    updating function pointert project

--- _cpp basic/function_pointer/main.cpp
+++ _cpp basic/function_pointer/main.cpp
@@ -10,11 +10,44 @@ using std::endl;
 using std::vector;
 using std::string; 
 
-//using namespace RoySpace;
+
+//* a function with void return type and no parameters
+void test_al() {
+	cout << "Hello, I am a test function alpha!" << endl;
+}
+
+//* a function with void return type and 2 parameters
+void test_be(int v_int, string v_str) {
+	cout << "Hello, I am a test function beta" << endl;
+	cout << v_int << ", " << v_str << endl;
+}
 
 int main()
 {
-    cout << "Hello World" << endl;
+	//* calling up function in a regular way
+	test_al();
+
+	//* a variable called p_testA, which is a pointer to function with a void return type and no parameters
+	void(*p_testA)();
+	void(*p_testB)();
+
+	//* syntax1
+	p_testA = &test_al;
+	(*p_testA)();
+
+	//* syntax2
+	p_testB = test_al;
+	p_testB();
+
+	//* syntax3
+	void(*p_testC)() = test_al;
+	p_testC();
+
+	//* a pointer to a function that take parameter type int and string and return void
+	void(*p_testD)(int, string) = test_be;
+	p_testD(8, "cool");
+
+
     return 0;
 }
 
