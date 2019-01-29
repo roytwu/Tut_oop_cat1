a2a9c72732e09cb46583a0d757db5bc9bd91bd89
roytwu <roywu2018@gmail.com>
2019-01-29 11:24:15 -0500

    adding function pointer project

---
+++ _cpp basic/function_pointer/main.cpp
@@ -0,0 +1,20 @@
+/* file name: main.cpp
+ * programmer: Roy T Wu
+ * Project: function pointer */
+
+#include <iostream>
+#include <vector>
+#include <string>
+using std::cout; 
+using std::endl;
+using std::vector;
+using std::string; 
+
+//using namespace RoySpace;
+
+int main()
+{
+    cout << "Hello World" << endl;
+    return 0;
+}
+
