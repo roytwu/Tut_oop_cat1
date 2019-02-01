de1b56fb4ad4673e2b875320f8c697d8dafc6cbd
roytwu <roywu2018@gmail.com>
2019-02-01 16:00:01 -0500

    minor update on the naming convention

---
+++ optional/nick_name/main.cpp
@@ -0,0 +1,59 @@
+/* File name: main.cpp
+ * Developer: Roy T Wu
+ * Project:   tutorial for std::optional, 
+              user name with an optional nickname and age
+ * Credit:    https://www.bfilipek.com/2018/05/using-optional.html
+ * Note:      std::optional is not incorporated in VS2015, here boost::optional is applied
+ */
+
+#include <iostream>
+#include <string>
+#include <boost/optional.hpp>
+
+using std::cout; 
+using std::endl;
+using std::string; 
+
+class UserRecord {
+public:
+	//* overloaded constructor
+	//* @para name: person's name
+	//* @para nickName: person's nick name wrapped by optional
+	//* @para age: person's age wrapped by optional
+	UserRecord(const string& name, const boost::optional<string> nickName, boost::optional<int> age)
+		: mName( name )
+		, mNickName( nickName )
+		, mAge( age )
+	{}
+
+	friend std::ostream & operator << (std::ostream& stream, const UserRecord& user);
+
+private:
+	string mName;
+	const boost::optional<string> mNickName;
+	boost::optional<int> mAge;
+};
+
+std::ostream& operator << (std::ostream& os, const UserRecord& user) {
+	os << user.mName << " ";
+	if (user.mNickName) {
+		os << *user.mNickName << " ";
+	}
+
+	if (user.mAge) {
+		os << "age of " << *user.mAge;
+	}
+	return os;
+}
+
+
+int main()
+{
+	//* only one level of conversion works, so "SuperTim" must be wrapped by std::string
+	UserRecord tim ( "Tim", string("SuperTim"), 16);
+	UserRecord nano { "Nathan", boost::none, boost::none };
+	cout << tim << endl;
+	cout << nano << endl;
+    return 0;
+}
+
