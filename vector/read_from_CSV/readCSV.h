9a9cbe44ef774474b70611af569c549c1e05f124
roytwu <roywu2018@gmail.com>
2019-02-05 15:17:39 -0500

    readCSV.hpp is renamed to readCSV.h

---
+++ vector/read_from_CSV/readCSV.h
@@ -0,0 +1,42 @@
+/* file name: readCSV.hpp
+*  programmer: Roy T Wu
+*  Project: reading data from a CSV file
+*  Reference: https://thispointer.com/how-to-read-data-from-a-csv-file-in-c/  */
+
+#ifndef READCSV_H_
+#define READCSV_H_
+
+#include <boost/algorithm/string.hpp>
+#include <fstream>
+#include <locale>
+
+#include "imuData.h"
+
+
+using std::ifstream;
+using std::getline;
+
+
+class CSVReader{
+public:
+	//* inline member initialization:
+	// CSVReader(string filename, string delim = ","): fileName(filename), delimeter(delim) {}
+	//* which is equivalent to:
+	// CSVReader(string filename, string delim = ","): {fileName=filename; delimeter=delim;}  
+	CSVReader(string&,  string&);
+
+	//* function to fetch data from a CSV file
+	vector< vector<string> > getData(); 
+	vector< IMUData > getData2();
+	vector<double> convertStrToDou(vector<string> svec); 
+
+private:	
+	string ms_fileName;
+	string ms_delimeter;
+};
+
+
+
+
+
+#endif  //READCSV_H_
