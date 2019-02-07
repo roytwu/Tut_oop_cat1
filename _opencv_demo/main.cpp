fa9114e81dbf0ac6e3ada3f61cbc1a33fb1500c2
roytwu <roywu2018@gmail.com>
2019-02-07 14:58:48 -0500

    adding OpenCV demo program

---
+++ _opencv_demo/main.cpp
@@ -0,0 +1,20 @@
+/* file name: main.cpp
+ * programmer: Roy T Wu
+ * Project: OpenCV demo*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+using std::cout; 
+using std::endl;
+using std::vector;
+using std::string; 
+
+
+int main()
+{
+    cout << "Hello World" << endl;
+
+    return 0;
+}
+
