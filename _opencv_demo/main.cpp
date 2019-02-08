69a203b77eac6069b2ccfc86c58b684fefc05ddd
roytwu <roywu2018@gmail.com>
2019-02-08 11:36:30 -0500

    updating CMakeLists with OpenCV feature

--- _opencv_demo/main.cpp
+++ _opencv_demo/main.cpp
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <opencv2/opencv.hpp>
+
 using std::cout; 
 using std::endl;
 using std::vector;
@@ -13,7 +15,9 @@ using std::string;
 
 int main()
 {
-    cout << "Hello World" << endl;
+	cv::Mat test(100, 100, CV_8U, cv::Scalar::all(150));
+	cv::imshow("test_window", test);
+	cv::waitKey(0); //wait infinitely until any keystroke in the window
 
     return 0;
 }
