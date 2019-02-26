be3db437b108c67d3a5d6ccdbc17f2efaf35ab78
roytwu <roywu2018@gmail.com>
2019-02-26 12:36:11 -0500

    adding tutorial of angle-axis class in the Eigen library

--- eigen_demo/main.cpp
+++ eigen_demo/main.cpp
@@ -9,7 +9,7 @@ using std::string;
 
 int main()
 {
-	//* ===== Matrix Class =====
+	cout << "===== Matrix Class =====" << endl;
 	//* declare a 3x3 matrix of double
 	Eigen::Matrix3d eye3 = Eigen::Matrix3d::Identity();
 	cout << eye3 << endl  << endl;
@@ -31,9 +31,10 @@ int main()
 
 	//* matrix transpose
 	Eigen::Matrix3d m1T = m1.transpose();
-	cout << m1T << endl;
+	cout << m1T << endl << endl;
 
-	//* ===== Quaternion Class =====
+
+	cout << "===== Quaternion Class =====" << endl;
 	//* quaternion for double
 	Eigen::Quaterniond q1(2, 0, 1, -3);
 	cout << "scalar part of the quaternion is ... " << q1.w() << endl;
@@ -41,6 +42,12 @@ int main()
 
 	//* length of the quaternion 
 	cout << "length of q1 is ... " << q1.norm() << endl << endl;
+
+	//* return unit quaternion
+	Eigen::Quaterniond uq1 = q1.normalized();
+	cout << "scalar part of the unit quaternion is ... " << uq1.w() << endl;
+	cout << "vector part of the unit quaternion is ... " << endl << uq1.vec() << endl;
+	cout << "length of uq1 is ... " << uq1.norm() << endl << endl;
 
 	//* quaternion identity
 	Eigen::Quaterniond eyeQ = Eigen::Quaterniond::Identity();
@@ -50,9 +57,28 @@ int main()
 	//* quaternion multiplication
 	Eigen::Quaterniond q2 = q1*eyeQ;
 
-	//* convert a quatternion to a rotation matrix
-	Eigen::Matrix3d r1 = eyeQ.toRotationMatrix();
-	cout << r1 << endl;
+	//* convert a unit quatternion to a rotation matrix
+	Eigen::Matrix3d r1 = uq1.toRotationMatrix();
+	cout << r1 << endl << endl;
 
+	cout << "===== Angle-axis Class =====" << endl;
+	//* angle-axis, note the axis must be normalized
+	Eigen::Vector3d axis1(0, 1, 0);
+	Eigen::AngleAxisd aa1(0, axis1);
+	
+	Eigen::Quaterniond q3;
+	q3 = aa1;   //* transfer from angle-axis representation to quaternion representation
+	//* Note that Eigen::Quaterniond q3 = aa; wont' work. In the C++ language, 
+	//*   this would require Transform to have a non-explicit conversion constructor from AngleAxis,
+	//*   but Eigen does not allow implicit casting here 
+
+	Eigen::AngleAxisd aa_foo;
+	aa_foo = q1; //* transfer from quaternion representation to angle-axis representation
+	//* notice that during this step, q1 will be normalized. 
+
+	cout << aa_foo.angle() << endl;
+	cout << aa_foo.axis()  << endl;
+	Eigen::Matrix3d r_foo = aa_foo.toRotationMatrix();
+	cout << r_foo << endl;
 }
 
