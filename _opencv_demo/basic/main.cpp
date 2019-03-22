cbb809c668869c3bff37c958be693d45c58f3d69
roytwu <roywu2018@gmail.com>
2019-03-22 11:40:24 -0400

    *adding comments *adding demo of hconcat() and vconcat()

--- _opencv_demo/basic/main.cpp
+++ _opencv_demo/basic/main.cpp
@@ -20,24 +20,24 @@ int main()
 	//cv::imshow("test_window", test);
 	//cv::waitKey(0); //wait infinitely until any keystroke in the window
 
-	//* make a 5x5 complex matrix filled with i+3j
+	//* --- make a 5x5 complex matrix filled with i+3j ---
 	//* CV_32FC2 means a 2-channel (complex) floating-point array
 	//* CV_8UC1 means a 8-bit single chaneel array
 	cv::Mat o_mat1(5, 5, CV_32FC2, cv::Scalar(1, 3));
 	cout << o_mat1 << endl << endl;
 
-	//* conver Mat reference to Mat_ reference
+	//* --- conver Mat reference to Mat_ reference ---
 	cv::Mat_<float>& o_matA = (cv::Mat_<float>&)o_mat1;
 	cout << o_matA << endl;
 
-	//* Mat::type and Mat::depth
+	//* --- Mat::type and Mat::depth ---
 	cout << "Type is..." <<	o_mat1.type() << endl;
 	cout << "Depth is..." << o_mat1.depth() << endl;
 	//* verify the depth of CV_32F
 	cout << CV_32F << endl;
 	cout << CV_32FC1 << endl;
 
-	cout << endl << "----- matrix multiplacation and transpose -----" << endl;
+	cout << endl << "----- matrix multiplacation and transpose -----\n";
 	float foo[2][2] = { {1, 0}, {0, 1} };
 	float bar[2][2] = { {22, 33}, {77, 88} };
 	cv::Mat o_matFoo = cv::Mat(2, 2, CV_32FC1, foo);
@@ -46,16 +46,16 @@ int main()
 	cv::Mat o_matToto = o_matBar.t();       //* matrix transpose
 	cout << o_matFB << endl << o_matToto << endl;
 
-	//* Matx are template class whose size are known at comilation time
-	cout << endl << "----- class Matx -----" << endl;
+	//* --- Matx are template class whose size are known at comilation time ---
+	cout << endl << "----- class Matx -----\n";
 	double arr[3][3] = { { 1.1, 1.2, 1.3 },{ 2.1, 2.2, 2.3 },{ 3.1, 3.2, 3.3 } };
 
 	//* Matx33d represents 3x3 matrix of double type
 	//* for double type array, use CV_64
 	cv::Matx33d o_matArr = cv::Mat(3, 3, CV_64FC1, arr); //* load the array to Matx33d
 	cv::Matx33d o_matArrInv = o_matArr.inv();            //* matrix inverse
-	cout << o_matArr << endl;
-	cout << o_matArrInv << endl << endl;
+	//cout << o_matArr << endl;
+	//cout << o_matArrInv << endl << endl;
 
 	//* cv::Matx with 4 by 4 float
 	cv::Matx44f o_xMF(11, 12, 13, 14,
@@ -67,28 +67,28 @@ int main()
 	cout << o_xMF.t() << endl;                 //* matrix transpose
 	//cout << o_xMF*o_xEye44 << endl << endl;    //* matrix multiplacation 
 
-	//* cross and dot product in vectors
-	cout << endl << "----- Mat::cross, Mat::dot -----" << endl;
+	//* --- cross and dot product in vectors ---
+	cout << endl << "----- Mat::cross, Mat::dot -----\n";
 	cv::Vec3d v_toto(1, 0.2, 0.7);
 	cv::Vec3d v_tata(0, -0.2, 1);
 	cout << v_toto.cross(v_tata) << endl; //*cross product
 	cout << v_toto.dot(v_tata) << endl;   //*dot product
 
-    //* access columns in Matx
-	cout << endl << "----- access columns in Matx -----" << endl;
+    //* --- access columns in Matx ---
+	cout << endl << "----- access columns in Matx -----\n";
 	cout << o_xMF.col(0) << endl;                //* c-out 1st column
 	cv::Matx41f o_mat2 = o_xMF.col(3);           //* 4th column, by default its a 4 by 1 matrix 
 	cv::Mat     o_mat3 = cv::Mat(o_xMF.col(3));  //* 4th column, change to cv::Mat format
-	cout << o_mat2 << endl;
-	cout << o_mat3 << endl << endl;
+	//cout << o_mat2 << endl;
+	//cout << o_mat3 << endl;
 
-	// colRange() and rowRange()
+	//* colRange() and rowRange()
 	cv::Mat o_mat4 = cv::Mat(o_xMF).colRange(0,3);  //* 1st to 3rd column, change to cv::Mat format
 	cv::Mat o_mat5 = o_mat4.rowRange(0,3);         
 	cout << o_mat5 << endl << endl;
 	
 
-	//* convert cv::Mat to std::vector
+	//* --- convert cv::Mat to std::vector ---
     std::vector<double> v_4thCol;
 	v_4thCol.assign(o_mat3.begin<float>(), o_mat3.end<float>());
 	for (auto i : v_4thCol) {
@@ -96,7 +96,8 @@ int main()
 	}
 	cout << endl;
 
-	cout << endl << "----- conversion between Eigen matrix and OpenCV matrix -----" << endl;
+
+	cout << "\n----- conversion between Eigen matrix and OpenCV matrix -----\n";
 	Eigen::MatrixXf em;       //* Eigen library, Dynamic float matrix 
 	cv::cv2eigen(o_xMF, em);
 	cout << em << endl << endl;
@@ -117,15 +118,22 @@ int main()
 	cout << rv_xPi << endl;
 
 
-	cout << endl << "----- cv::norm -----" << endl;
+	cout << "\n----- cv::norm -----\n";
 	//* vector norm, by default, normType is NORM_L2
 	cout << cv::norm(rv_xPi) << endl;  
-
+	
 	//* matrix norm, SO(3) shall have norm of 1
 	cout << cv::norm(mx_xPi, cv::NORM_INF) << endl;
 
 	
+	cout << "\n----- Matrix concatenation -----\n";
+	cv::Matx34d output1;
+    cv::hconcat(mx_xPi, rv_xPi, output1); //* horizontal concatenation 
+	cout << "Output of horizontal concatenation: \n" << output1 << endl << endl;
 
+	cv::Matx43d output2;
+	cv::vconcat(mx_xPi, rv_xPi.t(), output2); //* vertical concatenation 
+	cout << "Output of vertical concatenation: \n"  << output2 << endl;
     return 0;
 }
 
--- _opencv_demo/rotation/main.cpp
+++ _opencv_demo/rotation/main.cpp
@@ -1,7 +1,6 @@
 //* File name:   main.cpp
 //* Programmer:  Roy T Wu
-//* Description: Demonstrate mpping SO(3) to quaternion with OpenCV and Eigen
-//*              
+//* Description: Demonstrate mpping SO(3) to quaternion with OpenCV and Eigen        
 
 #include <iostream>
 #include <string>
@@ -11,34 +10,32 @@ using std::cout;
 using std::endl;
 using std::string; 
 
-
 int main()
 {
 	cv::Matx33d rm_eye33 = cv::Matx33d::eye();        //* identity matrix
 	cv::Matx33d rm_x_Pi(1, 0, 0, 0, -1, 0, 0, 0, -1); //* rotate pi along x-axis
-	
 	SO3Mapping o_mapping;
 
-	//* ---Step 1---, start from Euler angle, conver to SO(3)
+	//* ===== ===== Eigen version ===== =====
+	//* ---Step 1, start from Euler angle, conver to SO(3)
 	cv::Vec3d v_euler(0.5, CV_PI, 0.7);   //* CV_PI is the OpenCV defined constant
 	cv::Matx33d rm_euler = o_mapping.EulerToSO3(v_euler); //* create arbitrary rotation matrix
 	cout << rm_euler << endl << endl;
 
 	//* verify the mapping is legit
-	cout << "(Rotation matrix check) shall output identity matrix... " << endl;
+	cout << "(Rotation matrix check) shall output identity matrix... \n";
 	cv::Matx33d  result = rm_euler* rm_euler.t();
 	o_mapping.roundTinyDoubleToZero(result);
 	cout << result << endl << endl;
 	
-
-	//* ---Step 2---, map SO(3) to Eigen quaternion and OpenCV quaternion, respectively
+	//* ---Step 2,  map SO(3) to Eigen quaternion and OpenCV quaternion, respectively
 	Eigen::Quaterniond eigenQ = o_mapping.SO3ToEigenQuat(rm_euler);
 	o_mapping.printEigenQuat(eigenQ);
 	cout << endl;
 	cv::Vec4d v_quat = o_mapping.SO3ToCVQuat(rm_euler);
 	cout << v_quat << endl << endl;
 
-	//* ---Step 3---, convert both quaternions back to SO(3)
+	//* ---Step 3, convert both quaternions back to SO(3)
 	cv::Matx33d so3_A, so3_B;
 	so3_A = o_mapping.EGQuatToSO3(eigenQ);  
 	cout << so3_A << endl;
@@ -46,16 +43,15 @@ int main()
 	//* OpenCV
 	so3_B = o_mapping.CVQuatToSO3(v_quat); 
 	cout << so3_B << endl;
-
-	//* shall output identity
-	cout << "This shall output identiy.. \n" << so3_A*so3_B.t() << endl;
+	cout << "This shall output identiy.. \n" << so3_A*so3_B.t() << endl;  //* verfy the result
 
 
-	//* -creating SO(3) from Euler angle
+	//* ===== ===== OpenCV version ===== =====
+	//* ---Step 1, creating SO(3) from Euler angle
 	cv::Vec3d euler1(0, CV_PI, 0);
 	cv::Matx33d rm1 = o_mapping.EulerToSO3(euler1);
 	
-	//* mapping SO(3) to S(3) and then performing reverse map to  
+	//* ---Step 2, mapping SO(3) to S(3) and then performing reverse map to  
 	cv::Vec4d quat1 = o_mapping.SO3ToCVQuat(rm1);
 	cv::Matx33d rm11 = o_mapping.CVQuatToSO3(quat1);
 	cout << rm1*rm11.t() << endl;
