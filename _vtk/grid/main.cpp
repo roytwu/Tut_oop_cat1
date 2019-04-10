d21d23d7f2898ca50f9480f9e6fe4b1e149e8af5
roytwu <roywu2018@gmail.com>
2019-04-10 15:39:53 -0400

    adding VTK project 'grid'

---
+++ _vtk/grid/main.cpp
@@ -0,0 +1,33 @@
+//* File name:   main.cpp
+//* Developer:   Roy T Wu
+//* Description: first program about VTK   
+//*              code origin: https://lorensen.github.io/VTKExamples/site/Cxx/GeometricObjects/Axes/
+                
+#include <iostream>
+#include <string>
+
+#include <vtkActor.h>
+#include <vtkAxesActor.h>
+#include <vtkCamera.h>
+#include <vtkCaptionActor2D.h>
+#include <vtkNamedColors.h>
+#include <vtkPolyData.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkSmartPointer.h>
+#include <vtkSphereSource.h>
+#include <vtkTextProperty.h>
+#include <vtkTransform.h>
+
+using std::cout; 
+using std::endl; 
+
+int main()
+{
+
+
+
+	return 0;
+}
