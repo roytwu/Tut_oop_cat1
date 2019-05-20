28aac101eaf4502f33b3a452db70073f4c699fff
roytwu <roywu2018@gmail.com>
2019-05-20 17:03:42 -0400

    adding new project and rearranging file structure

---
+++ operator_overload/functionCall_operator/main.cpp
@@ -0,0 +1,22 @@
+/* *********************************************************
+File name:   main.cpp (overloading function-call operator)
+Developer:   Roy Wu
+Last upate:  05/20/2019
+Description: 
+********************************************************* */
+
+#include <iostream>
+#include <string>
+
+using std::cout; 
+using std::endl;
+using std::string;
+
+
+int main(){
+	
+
+	
+	return 0;
+}
+
