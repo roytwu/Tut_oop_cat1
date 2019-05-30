9ec3a529a9f1bd8191f895a965c1fdac1b8f31bc
roytwu <roywu2018@gmail.com>
2019-05-30 11:38:29 -0400

    adding new utility project

---
+++ utility/move/main.cpp
@@ -0,0 +1,24 @@
+//* File name:   main.cpp
+//* Programmer:  Roy T Wu
+//* Description: building blocks for Kalman filter 
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <Eigen/Core>              //* this needs to added before <opencv2/core/eigen.hpp>
+#include <Eigen/Dense>             //* Eigen library: Matrix
+#include <Eigen/Geometry>          //* Eigen library: quaternion
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/eigen.hpp> 
+using std::cout; 
+using std::endl;
+using std::string; 
+
+
+
+int main()
+{
+
+    return 0;
+}
+
