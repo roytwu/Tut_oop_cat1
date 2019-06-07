1e325da1ad81fa8a8624153f3f7e20c7ddd04a79
roytwu <roywu2018@gmail.com>
2019-06-07 15:39:15 -0400

    adding porject 'poly'

---
+++ _vtk/poly/main.cpp
@@ -0,0 +1,25 @@
+/* *********************************************************
+File name:   main.cpp (_polyData)
+Programmer:  Roy Wu
+Description: example of vtkPolyData 
+********************************************************* */
+
+//#include <array>               //* std::array
+#include <iostream>
+#include <string>
+#include <vtkMath.h>
+#include <vtkPoints.h>
+#include <vtkSmartPointer.h>
+
+using std::cout; 
+using std::endl;
+using std::string; 
+
+
+int main()
+{
+
+	
+
+	return 0;
+}
