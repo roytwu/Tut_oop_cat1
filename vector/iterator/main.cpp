ba5e934819d4b6d603747073b58c511d8e4ae6e1
roytwu <roywu2018@gmail.com>
2019-06-07 11:17:01 -0400

    adding project 'iterator'

---
+++ vector/iterator/main.cpp
@@ -0,0 +1,27 @@
+/* *********************************************************
+File name:   main.cpp (_iterator)
+Programmer:  Roy Wu
+Description: basic concepts and syntax about vectors
+********************************************************* */
+
+#include <iostream>
+#include <vector>
+#include <algorithm> //* copy
+#include <iterator>  //* ostream_iterator
+#include <string>    //* to avoid error C2679
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+
+vector<string> func_GetString();
+
+
+int main(){
+
+
+	return 0;
+}
+
+
