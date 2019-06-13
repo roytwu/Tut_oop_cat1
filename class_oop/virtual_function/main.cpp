8d1c85a453dc98882e5c1735022ad1c85d8c74aa
roytwu <roywu2018@gmail.com>
2019-06-13 11:32:25 -0400

    adding project 'virtual_function'

---
+++ class_oop/virtual_function/main.cpp
@@ -0,0 +1,25 @@
+/* *********************************************************
+File name:   main.cpp 
+Programmer:  Roy Wu
+Description: 
+********************************************************* */
+
+//#include <array>               //* std::array
+#include <iostream>
+#include <string>
+#include <vtkMath.h>
+#include <vtkPoints.h>
+#include <vtkSmartPointer.h>
+
+using std::cout; 
+using std::endl;
+
+
+
+int main()
+{
+
+	
+
+	return 0;
+}
