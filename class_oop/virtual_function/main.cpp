7b1cb3670155e2f8bf964d6cfe41abc4c1e1561c
roytwu <roywu2018@gmail.com>
2019-06-13 12:27:40 -0400

    adding demo about runtime polumorphism

--- class_oop/virtual_function/main.cpp
+++ class_oop/virtual_function/main.cpp
@@ -3,7 +3,6 @@ File name:   main.cpp
 Programmer:  Roy Wu
 Description: 
 ********************************************************* */
-
 //#include <array>               //* std::array
 #include <iostream>
 #include <string>
@@ -14,12 +13,40 @@ Description:
 using std::cout; 
 using std::endl;
 
+class Base 
+{
+public:
+	virtual void print() { cout << "print base class\n"; }
+	        void show()  { cout << "show base class\n"; }
+};
+
+class Derived : public Base
+{
+public:
+	//* print() is already a virtual function in derived class
+	void print() {
+		cout << "print drived class\n";
+	}
+
+	void show() {
+		cout << "show derived class\n";
+	}
+};
 
 
 int main()
 {
+	Base    *ptr_base;
+	Derived o_d1;
 
-	
+	//* prt_base conatins the address of object o_d1
+	ptr_base = &o_d1;
+
+	//* runtime polymorphism: virtual function is binded at runtime
+	ptr_base->print();
+
+	//* non-virutal function, binded at compile time
+	ptr_base->show();
 
 	return 0;
 }
