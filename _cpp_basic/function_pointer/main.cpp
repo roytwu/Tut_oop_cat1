307c8a5db088dd5dd9ae78cada223af2a2cb477e
roytwu <roywu2018@gmail.com>
2019-06-18 14:52:46 -0400

    updating comments

--- _cpp_basic/function_pointer/main.cpp
+++ _cpp_basic/function_pointer/main.cpp
@@ -1,7 +1,8 @@
-/* file name: main.cpp
- * programmer: Roy T Wu
- * Project: function pointer */
-
+ /* *********************************************************
+ File name:   main.cpp (_funcPtr)
+ Programmer:  Roy Wu
+ Description: basic concepts of funciton pointer
+ ********************************************************* */
 #include <iostream>
 #include <vector>
 #include <string>
@@ -12,39 +13,39 @@ using std::string;
 
 
 //* a function with void return type and no parameters
-void test_al() {
-	cout << "Hello, I am a test function alpha!" << endl;
+void test_alpha() {
+	cout << "Hello, I am test function alpha!" << endl;
 }
 
 //* a function with void return type and 2 parameters
-void test_be(int v_int, string v_str) {
-	cout << "Hello, I am a test function beta" << endl;
+void test_beta(int v_int, string v_str) {
+	cout << "Hello, I am test function beta" << endl;
 	cout << v_int << ", " << v_str << endl;
 }
 
 int main()
 {
 	//* calling up function in a regular way
-	test_al();
+	test_alpha();
 
 	//* a variable called p_testA, which is a pointer to function with a void return type and no parameters
 	void(*p_testA)();
 	void(*p_testB)();
 
 	//* syntax1
-	p_testA = &test_al;
+	p_testA = &test_alpha;
 	(*p_testA)();
 
 	//* syntax2
-	p_testB = test_al;
+	p_testB = test_alpha;
 	p_testB();
 
 	//* syntax3
-	void(*p_testC)() = test_al;
+	void(*p_testC)() = test_alpha;
 	p_testC();
 
 	//* a pointer to a function that take parameter type int and string and return void
-	void(*p_testD)(int, string) = test_be;
+	void(*p_testD)(int, string) = test_beta;
 	p_testD(8, "cool");
 
 
