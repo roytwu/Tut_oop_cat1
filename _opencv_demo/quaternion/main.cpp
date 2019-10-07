28b6ed46f8a30e8045afcedbd19357807ae7fb6a
roytwu <roywu2018@gmail.com>
2019-10-07 14:30:49 -0400

    mapping back and forth between SO(3) and other parameterizations

--- _opencv_demo/quaternion/main.cpp
+++ _opencv_demo/quaternion/main.cpp
@@ -18,67 +18,61 @@ int main()
 	SO3Mapping o_mapping;
 
 	//* ### start from Euler angle, convert to SO(3)
-	cv::Vec3d v_toto(0, CV_PI, 0.5*CV_PI);  
-	cv::Vec3d v_tata(CV_PI, 0., 0.0);
+	cv::Vec3d v_toto(0, -0.1, -0.5*CV_PI);  
+	cv::Vec3d v_tata(0., 0.5*CV_PI, 0.);
 	cv::Matx33d rm_toto = o_mapping.EulerToSO3(v_toto); 
 	cv::Matx33d rm_tata = o_mapping.EulerToSO3(v_tata);
-    cout << "rm_toto is ...\n" << rm_toto << endl;
-    cout << "rm_tata is ...\n" << rm_tata << endl;
+    cout << "rm_toto is (from Euler angles)...\n" << rm_toto << endl;
+    cout << "rm_tata is (from Euler angles)...\n" << rm_tata << endl;
 
     //* ### SO(3) <---> Rotation vector
     cv::Vec3d rotVec_toto;
     cv::Vec3d rotVec_tata;
-    cv::Rodrigues(rm_toto, rotVec_toto);
+    cv::Rodrigues(rm_toto, rotVec_toto); //* SO3->rotVec
     cv::Rodrigues(rm_tata, rotVec_tata);
     cout << "\nrotVec_toto (from SO(3)) is ...\n" << rotVec_toto << endl;
-    cout << "\nrotVec_tata (from SO(3)) is ...\n" << rotVec_tata << endl;
+    cout << "rotVec_tata (from SO(3)) is ...\n" << rotVec_tata << endl;
 
 	//* ### SO(3) <---> quaternion 
-	cout << "\n*** quaternion in CV: \n";
-	cv::Vec4d quat_toto = S3::SO3ToCVQuat(rm_toto);
+	cv::Vec4d quat_toto = S3::SO3ToCVQuat(rm_toto); //* SO3->quat
 	cv::Vec4d quat_tata = S3::SO3ToCVQuat(rm_tata);
     cout << "\nquat_toto (from SO(3)) is ...\n" << quat_toto << endl;
-    cout << "\nquat_tata (from SO(3)) is ...\n" << quat_tata << endl;
+    cout << "quat_tata (from SO(3)) is ...\n" << quat_tata << endl;
 	
-    //* ### quaternion operation
-	cv::Vec4d output1;
-	cv::Vec4d output2;
-	cv::Vec4d output3;
+ //   //* ### quaternion operation
+	//cv::Vec4d output1;
+	//cv::Vec4d output2;
+	//cv::Vec4d output3;
 
-	output1 = S3::quatMultiplication(quat_toto, quat_tata);
-	cout << "\nquatMultiplication ...\n"<< output1 << endl;
-
-	output2 = S3::rightToLeft(quat_tata) * quat_toto;
-	//output2 = cv::Mat(S3::rightToLeft(quat_tata)) * cv::Mat(quat_toto);
-	cout << output2 << endl;
-
-	output3 = S3::quatMultMatx(quat_toto) * quat_tata;
-	cout << output3 << endl;
-
-
+	//output1 = S3::quatMultiplication(quat_toto, quat_tata);
+ //   output2 = S3::rightToLeft(quat_tata) * quat_toto;
+ //   output3 = S3::quatMultMatx(quat_toto) * quat_tata;
+	//cout << "\nquatMultiplication ...\n"<< output1 << endl;
+	//cout << output2 << endl;
+	//cout << output3 << endl;
 
     //* ### SO(3) <---> angle axis 
-    cv::Vec4d rod_toto = S3::SO3ToRodrigues(rm_toto);
+    cv::Vec4d rod_toto = S3::SO3ToRodrigues(rm_toto);  //* SO3->Rod
     cout << "\nrod_toto (from SO(3)) is ...\n" << rod_toto << endl;
     cv::Vec4d rod_tata = S3::SO3ToRodrigues(rm_tata);
-    cout << "\nrod_tata (from SO(3)) is ...\n" << rod_tata << endl;
+    cout << "rod_tata (from SO(3)) is ...\n" << rod_tata << endl;
 
-    //cv::Matx33d rm_toto2 = S3::rodriguesToSO3(rod_toto);
-    //cout << "\nrm_toto2 (from Rodrigues) is ...\n" << rm_toto2 << endl;
-    //cv::Matx33d rm_tata2 = S3::rodriguesToSO3(rod_tata);
-    //cout << "\nrm_tata2 (from Rodrigues) is ...\n" << rm_tata2 << endl;
+    cv::Matx33d rm_toto2 = S3::rodriguesToSO3(rod_toto); //* SO3->Rod->SO3
+    cout << "\nrm_toto2 (from Rodrigues) is ...\n" << rm_toto2 << endl;
+    cv::Matx33d rm_tata2 = S3::rodriguesToSO3(rod_tata);
+    cout << "rm_tata2 (from Rodrigues) is ...\n" << rm_tata2 << endl;
 
-    ////* ### quaternion <---> angle axis
-    //cv::Vec4d rod_toto2 = S3::quatToRodrigues(quat_toto);
-    //cout << "\nrod_toto2 (from quat) is ...\n" << rod_toto2 << endl;
+    //* ### quaternion <---> angle axis
+    cv::Vec4d rod_toto2 = S3::quatToRodrigues(quat_toto);  //* SO3->quat->Rod
+    cout << "\nrod_toto2 (from quat) is ...\n" << rod_toto2 << endl;
 
-    //cv::Vec4d rod_tata2 = S3::quatToRodrigues(quat_tata);
-    //cout << "\nrod_tata2 (from quat) is ...\n" << rod_tata2 << endl;
+    cv::Vec4d rod_tata2 = S3::quatToRodrigues(quat_tata);
+    cout << "\nrod_tata2 (from quat) is ...\n" << rod_tata2 << endl;
 
-    //cv::Vec4d quat_toto2 = S3::rodriguesToQuat(rod_toto2);
-    //cout << "\nquat_toto2 (from Rodrigues) is ...\n" << quat_toto2 << endl;
-    //cv::Vec4d quat_tata2 = S3::rodriguesToQuat(rod_tata2);
-    //cout << "\nquat_tata2 (from Rodrigues) is ...\n" << quat_tata2 << endl;
+    cv::Vec4d quat_toto2 = S3::rodriguesToQuat(rod_toto2);  //* SO3->quat->Rod->quat
+    cout << "\nquat_toto2 (from Rodrigues) is ...\n" << quat_toto2 << endl;
+    cv::Vec4d quat_tata2 = S3::rodriguesToQuat(rod_tata2);
+    cout << "\nquat_tata2 (from Rodrigues) is ...\n" << quat_tata2 << endl;
 
 
 
