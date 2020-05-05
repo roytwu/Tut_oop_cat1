7f9c363dfdc14871103d8359509c5c463ff65f1d
roytwu <roywu2018@gmail.com>
2020-05-05 15:20:40 -0400

    renaming files

--- vector/gcd/_unitTest/main.cpp
+++ vector/gcd/_unitTest/main.cpp
@@ -10,26 +10,33 @@ Description:
 #include <Eigen/Geometry>          //* Eigen library: quaternion
 #include <opencv2/opencv.hpp>
 #include <opencv2/core/eigen.hpp> 
-#include "gcd.hpp"
+#include "gcd.hpp"                 //* test target
 //#include <boost/algorithm/string.hpp>
 //using std::cout; 
 //using std::endl;
 //using std::string; 
 //using std::vector;
 
-#define BOOST_TEST_MODULE MyTest
+#define BOOST_TEST_MODULE testGCD
 //#include <boost/test/unit_test.hpp>        //* not working (?)
 #include <boost/test/included/unit_test.hpp> //* this works
 
-//* ----- ----- ----- -----
-//* dectect and report err in the add() function
-int add(int i, int j) { return i + j; }
-//* ----- ----- ----- -----
 
 BOOST_AUTO_TEST_CASE(my_test)
 {
+    //* 1st test case, 5 elements
+    int testArr1[] = { 4, 9, 40, 5, 67 };
+    
+    //* 2nd test cases, 6 elements
+    int testArr2[] = {16, 12, 4, 80, 52, 84};
+    
+    //* 3rd test cases, 7 elements
+    int testArr3[] = {30, 15, 35, 75, 10, 20, 100 };
+    
+
+
     //* ---seven ways to detect and report the same error:
-    BOOST_CHECK(add(2, 2) == 4);          //* #1 continues on error
+    BOOST_CHECK(findGCD(2, 2) == 4);          //* #1 continues on error
 
     BOOST_REQUIRE(add(2, 2) == 4);        //* #2 throws on error
 
